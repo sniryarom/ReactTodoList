14ee7a63edd7941b4deb50e5983a1fa815e768df
snirya <snir@homex.co.il>
2017-02-21 00:39:01 +0700

    before implement delete

--- src/App.js
+++ src/App.js
@@ -1,52 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import styles from './AppStyles.css'; // Tell Webpack that Button.js uses these styles
 
- class App extends React.Component {
-  constructor(){
-     super();
-     this.state = {text: '', list: []}
-     this.update = this.update.bind(this)
-     this.addItem = this.addItem.bind(this)
-   }
-   update(e){
-      this.setState({text: e.target.value})
-     
-   }
-   addItem(){
-    if (this.state.text != '') {
-      var newArray = this.state.list.slice();    
-      newArray.push({text: this.state.text});   
-      this.setState({list:newArray})
-      this.setState({text: ''})
-    }
-    
-  }
-
-  unmount(){
-    ReactDOM.unmountComponentAtNode(document.getElementById('todoList'))
-  }
-
-  // shouldComponentUpdate() {
-  //   return false;
-  // }
-  
-  render(){
-     console.log('App render');
-     return (
-       <div>
-        <input type="text" style={inputBtnStyle} value={this.state.text} onChange={this.update} />
-        <button onClick={this.addItem}>ADD</button>
-          <hr/>
-          <div>
-            <h1>ToDo List</h1>
-            <TodoList list={this.state.list} />
-          </div>
-       </div>
-     )
-   }
- } 
-
- const checkboxHideStyle = {
+const checkboxHideStyle = {
   display: 'none'
 };
 
@@ -64,31 +20,91 @@ const todoListTypeStyle = {
 
 const spanTextStrikeThroughStyle = {
   textDecoration: 'line-through'
-}
+};
 
 const spanTextRegularStyle = {
   textDecoration: 'none'
-}
+};
 
 const deleteBtnStyle = {
-  marginLeft: '30px'
+  margin: '30px'
 }
 
 
- class TodoList extends React.Component {
 
+ class App extends React.Component {
   constructor(){
      super();
+     this.state = {text: '', textList: []}
+     this.update = this.update.bind(this)
+     this.addItem = this.addItem.bind(this)
+   }
+   update(e){
+      this.setState({text: e.target.value})
+      //this.setState({toRender: false})
+   }
+   addItem(){
+    if (this.state.text !== '') {
+      var newArray = this.state.textList.slice();    
+      newArray.push({text: this.state.text});   
+      this.setState({textList: newArray})
+      this.setState({text: ''})
+    }
+    
+  }
+
+  handleKeyPress = (event) => {
+  if(event.key == 'Enter'){
+   this.addItem();  
+  }
+}
+
+  // shouldComponentUpdate() {
+  //    return this.state.toAdd;
+  //  }
+  
+  render(){
+     console.log('App render');
+     return (
+       <div>
+        <input type="text" style={inputBtnStyle} value={this.state.text} onChange={this.update} onKeyPress={this.handleKeyPress} />
+        <button onClick={this.addItem} >ADD</button>
+          <hr/>
+          <div>
+            <h1>ToDo List</h1>
+            <TodoList list={this.state.textList} />
+          </div>
+       </div>
+     )
+   }
+ } 
+
+ 
+
+/**
+*
+*/
+ class TodoList extends React.Component {
+
+  constructor(props){
+     super(props);
     this.removeItem = this.removeItem.bind(this)
   }
 
   removeItem(e) {
     console.log('remove item clicked')
-    var array = this.props.list;
+    var array = this.state.list;
     var index = array.indexOf(e.target.value)
     array.splice(index, 1);
-    this.setState({people: array });
+    this.setState({list: array });
   }
+
+  //  componentWillReceiveProps(){
+  //    console.log('TodoItem componentWillReceiveProps');
+  //    var newArray = this.state.list.slice();    
+  //     newArray.push({text: this.props.text});   
+  //     this.setState({list:newArray})
+  // }
 
   render(){
     console.log('TodoList render');
@@ -97,8 +113,8 @@ const deleteBtnStyle = {
         
         <ul id="todoList" style={todoListTypeStyle}>
         {
-            this.props.list.map((item) => (
-                <TodoItem text={item.text} removeItemFunc={this.removeItem}/>
+            this.props.list.map((item, index) => (
+                <TodoItem key={index} text={item.text} removeItemFunc={this.removeItem}/>
             ))
         }    
         </ul>
@@ -109,6 +125,7 @@ const deleteBtnStyle = {
   }
 }
 
+
 class TodoItem extends React.Component {
    // componentWillMount(){
    //  console.log('TodoItem componentWillMount');
@@ -116,10 +133,10 @@ class TodoItem extends React.Component {
    constructor(){
      super();
      this.state = {style: spanTextRegularStyle}
-     this.addItem = this.addItem.bind(this)
+     this.checkItem = this.checkItem.bind(this)
    }
 
-   addItem(e){
+   checkItem(e){
       console.log('add item clicked')
       if (e.target.checked) {
         this.setState({style: spanTextStrikeThroughStyle})
@@ -133,7 +150,7 @@ class TodoItem extends React.Component {
     console.log('TodoItem render');
      return (
        <li>
-            <input type="checkbox" onClick={this.addItem} />
+            <input type="checkbox" onClick={this.checkItem} />
             <span style={this.state.style}>{this.props.text}</span>
             <a href='' style={deleteBtnStyle} onClick={this.props.removeItemFunc}>delete</a>
        </li>
